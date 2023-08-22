fa900d424b703ec0547acf742a6e836c18d899ee
emincingoz <emincingoz37@gmail.com>
2023-08-22 21:20:09 +0300

    Authority empty template

---
+++ src/test/java/com/readnet/readnetauthority/ReadnetAuthorityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readnet.readnetauthority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadnetAuthorityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
